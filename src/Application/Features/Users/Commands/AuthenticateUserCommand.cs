a6cc17ef0add52197ea2ff5d07948749ab81eed2
troyvinson <troyvinson@gmail.com>
2023-05-29 17:28:09 -0500

    remove user auth and register in favor of B2C

--- src/Application/Features/Users/Commands/AuthenticateUserCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Identity;
-
-namespace Application.Features.Users.Commands;
-
-public sealed record AuthenticateUserCommand(UserForAuthenticationDto User) : IRequest<TokenDto>;
--- src/Application/Features/Users/Commands/RegisterUserCommand.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Identity;
-
-namespace Application.Features.Users.Commands;
-
-public sealed record RegisterUserCommand(UserForRegistrationDto User) : IRequest<IdentityResult>;
